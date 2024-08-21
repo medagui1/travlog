001198330983af4d016797625570b096b8b6fd2c
Med Agui <simohamedregragui2@gmail.com>
2024-08-21 23:11:23 +0100

    adding max-width to the services and destinations cards

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCard = ({item}) => {
   return (
-    <figure className='rounded-[32px] bg-white p-wide flex flex-col justify-center items-center gap-wide'>
+    <figure className='rounded-[32px] bg-white p-wide flex flex-col justify-center items-center gap-wide max-w-[400px]' >
 <img src={item.img} alt="" />
 <h3 className='text-[24px] font-[500] '> {item.title} </h3>
 <p className='text-p-50 font-fs-p '> {item.description} </p>
