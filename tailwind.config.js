1cdddbe9e1d194bdff00fdf384c21ad4387cfaa0
Med Agui <simohamedregragui2@gmail.com>
2024-08-21 13:06:58 +0100

    adding custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        primary : '#F85E9F',
+        secondary : '#222831',
+        p : '#19182550'
+      },
+      fontSize : {
+        'h1' : '40px',
+        'fs-p' : '16px'
+      },
+      spacing : {
+        wider : '40px',
+        thin : '16px',
+        wide : '32px',
+        normal : '24px',
+        
+      }
+    },
   },
   plugins: [],
 }
