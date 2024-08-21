0b9d4bdf40350dcb36f6192a44bd25473c7fc433
Med Agui <simohamedregragui2@gmail.com>
2024-08-21 13:37:15 +0100

    adding the services section

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import "./App.css";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Clients from "./sections/Clients";
+import Services from "./sections/Services";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="circular">
         <Hero />
         <Clients />
+        <Services />
       </main>
     </>
   );
